ad53856601c0dcb4702a891f8eab2ed33a2ee3d2
lingchen2 <289140347@qq.com>
2020-02-21 23:42:30 +0800

    update log dir

--- nCov/log/savelog.py
+++ nCov/log/savelog.py
@@ -4,7 +4,7 @@ import logging
 def logs(logname):
     logger = logging.getLogger("%s" % logname)
     logger.setLevel(level=logging.DEBUG)
-    filehandler = logging.FileHandler('./log/%s.txt' % logname)
+    filehandler = logging.FileHandler('./nCov/log/%s.txt' % logname)
     famatter = logging.Formatter("%(asctime)s-%(name)s-%(levelname)s-%(message)s")
     filehandler.setFormatter(famatter)
     logger.addHandler(filehandler)
