52b6223776c1fd4ff5bf3b0a007bd8724d03c9d8
lingchen2 <289140347@qq.com>
2020-02-24 21:48:54 +0800

    add img

--- nCov/SendEmail/sendwe.py
+++ nCov/SendEmail/sendwe.py
@@ -1,17 +1,28 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 
 class SM:
-    def __init__(self, sender, receiver, password, message):
+    def __init__(self, sender, receiver, password, message, imgpath):
         self.sender = sender
         self.receiver = receiver
         self.password = password
         self.message = message
+        self.imgpath = imgpath
 
     def sendEmail(self):
-        content = MIMEText(self.message, 'html', 'utf-8')
+        msgtext = MIMEText(self.message + '''<table><tr><td><img src="cid:ache"></td></tr></table>''', 'html', 'utf-8')
+        msgAlternative = MIMEMultipart('alternative')
+        content = MIMEMultipart('related')
+        content.attach(msgAlternative)
+        msgAlternative.attach(msgtext)
+        with open(self.imgpath, "rb") as ig:
+            msgimg = MIMEImage(ig.read())
+        msgimg.add_header('Content-ID', 'ache')
+        content.attach(msgimg)
         subject = '每日一发-疫情进展'
         content['Subject'] = Header(subject, 'utf-8')
         content['From'] = Header('LingChen', 'utf-8')
@@ -19,3 +30,5 @@ class SM:
         smtp.login(self.sender, self.password)
         smtp.sendmail(self.sender, self.receiver, content.as_string())
         smtp.quit()
+
+
